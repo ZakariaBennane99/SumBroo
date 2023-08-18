50d1325bc9f315ee059f9ce52346ce1146fcc6d1
Zakaria Bennane <zakariamonre@gmail.com>
2023-08-18 21:15:55 +0100

    passwordPage

--- src/pages/onboarding/[token].js
+++ src/pages/onboarding/[token].js
@@ -18,9 +18,13 @@ const Onboarding = ({ userId, status }) => {
 
   const router = useRouter();
 
+  const [pass, setPass] = useState('')
+  const [confirPass, setConfirmPass] = useState('')
+  const [passErrors, setPassErrors] = useState(null)
+
   // if onboarding step is 2, take the user to the last step which is 
   // to link the accounts in the settings
-  if (status !== 'password' || status !== 'payment') {
+  if (!['password', 'payment'].includes(status)) {
     router.push('/sign-in');
     return null; // Return null to prevent rendering the component
   }
@@ -57,13 +61,38 @@ const Onboarding = ({ userId, status }) => {
   }
 
   async function handlePassword() {
+    if (pass != confirPass) {
+      setPassErrors(['Passwords do not match'])
+      return
+    }
+    setIsLoading(true)
+    try {
+      const response = await fetch('http://localhost:4050/api/set-up-password', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ userId, pass })
+      });
 
+      const data = await response.json();
+      if (data.errors) {
+        setPassErrors(data.errors.map(er => er.msg))
+        setIsLoading(false)
+      } else {
+        console.log(data)
+      }
+
+    } catch (error) {
+      setIsError(true)
+      console.error('Server Error');
+    }
   }
 
 
   const customStyles = {
     content: {
-      width: '55%',
+      width: '20%',
       top: '50%',
       left: '50%',
       right: 'auto',
@@ -90,15 +119,23 @@ const Onboarding = ({ userId, status }) => {
           status === 'password' ? 
             <div className='password-container'>
               <h1>Step 1: Set Up a Password</h1>
-              <div>
-                <div>
-                  <label>Password</label>
-                  <input />
+              <div className='pass-holder'>
+                {
+                  passErrors ? passErrors.map(err => <p style={{ width: '100%', textAlign: 'center', marginBottom: '15px',
+                  marginTop: '0px', color: "red", fontSize: '.85em' }}>{err}</p>)  : ''
+                }
+                <div className='pass'>
+                  <label htmlFor='pass'>Password</label>
+                  <input type='password' id='pass' placeholder='Enter your new password' 
+                  onChange={(e) => { setPassErrors(null); setPass(e.target.value)}} />
                 </div>
-                <div>
-                  <label>Confirm Password</label>
-                  <input />
+                <div className='confirm-pass'>
+                  <label htmlFor='confirm-pass'>Confirm Password</label>
+                  <input type='password' id='confirm-pass' placeholder='Confirm your password'
+                  onChange={(e) => { setPassErrors(null); setConfirmPass(e.target.value) }}/>
                 </div>
+
+                <button disabled={isLoading ? true : false} className={`button ${isLoading ? 'loading' : ''}`} onClick={(handlePassword)}>{isLoading ? <Tadpole width={20} color='white' /> : 'Confirm'}</button>
               </div>
             </div>
           : status === 'payment' ?
@@ -106,7 +143,7 @@ const Onboarding = ({ userId, status }) => {
               <h1>Step 2: Let's Take Care of the Payment</h1>
               <div style={{ width: '100%', position: 'relative' }}>
                 <button type='button' className={`button ${isLoading ? 'loading' : ''}`} onClick={handlePayment} disabled={isLoading ? true : false}>
-                  {isLoading ? <Tadpole width={25} color='white' /> : <>Pay Via Stripe <img src='/pinterest/external-white.svg' /></>}</button>
+                  {isLoading ? <Tadpole width={20} color='white' /> : <>Pay Via Stripe <img src='/pinterest/external-white.svg' /></>}</button>
               </div>
             </div> 
           : 
@@ -124,15 +161,20 @@ const Onboarding = ({ userId, status }) => {
           onRequestClose={closeModal}
           contentLabel="Example Modal"
             >
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <h2 style={{ fontFamily: 'Ubuntu', fontSize: '1.3em', color: '#1c1c57' }} >Server Error</h2>
             <span onClick={() => location.reload()}
               style={{ backgroundColor: '#1465e7', 
               color: "white",
-              padding: '10px 0px 10px 0px', 
+              padding: '10px', 
               cursor: 'pointer',
               fontFamily: 'Ubuntu',
-              fontSize: '1.1em' }}>Try again</span>
+              borderRadius: '3px',
+              fontSize: '1.1em',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+               }}>Try again</span>
           </div>
         </Modal>
 
@@ -146,7 +188,6 @@ export default Onboarding;
 
 
 export async function getServerSideProps(context) {
-  console.log('runnign')
 
   try {
 
@@ -154,17 +195,14 @@ export async function getServerSideProps(context) {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log((decoded.action !== 'onboarding' && decoded.action === 'payment') || 
-    ((decoded.action === 'onboarding' && decoded.action !== 'payment')))
-    if (decoded.action !== 'onboarding') {
+    if (!['onboarding', 'password'].includes(decoded.action)) {
       throw new Error('Invalid action');
     }
-    console.log('first')
+
     const userId = decoded.userId
     await connectUserDB()
     if (decoded.action === 'onboarding') { 
       // assuming onboardingStep is 0
-      console.log('second')
       const sanitizedUserId = mongoSanitize.sanitize(userId);
       let user = await userDbConnection.model('User').findOne({ _id: sanitizedUserId });
       if (!user || user.onboardingStep !== 0) throw new Error('User not found');
@@ -176,7 +214,6 @@ export async function getServerSideProps(context) {
     }
 
     if (decoded.action === 'payment') {
-      console.log('third')
       const sanitizedUserId = mongoSanitize.sanitize(userId);
       let user = await userDbConnection.model('User').findOne({ _id: sanitizedUserId });
       if (!user || user.onboardingStep !== 1) throw new Error('User not found');
