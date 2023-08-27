919fad6f7fd724a4bea7e51bf0a3051fe389ecc4
Zakaria Bennane <zakariamonre@gmail.com>
2023-08-27 20:36:28 +0300

    .

--- src/pages/settings/linked-accounts/index.js
+++ src/pages/settings/linked-accounts/index.js
@@ -74,7 +74,6 @@ const LinkedAccounts = ({ AllAccounts, newUser, isServerErr, userId }) => {
                     return (
                     <div className="smAccountsContainer">
                         <div className="linkedAccountsWrapper"> 
-    
                             <div className="linkedAccounts">
                                 <div className="account">
                                     <span id="sm"><img id="smlg" src={`/sm/${acc.name}.svg`} /> {capitalize(acc.name)}</span>
@@ -119,11 +118,12 @@ export async function getServerSideProps(context) {
 
   const { connectUserDB, userDbConnection } = require('../../../../utils/connectUserDB');
   const jwt = require('jsonwebtoken');
-  const mongoSanitize = require('express-mongo-sanitize')
+  const mongoSanitize = require('express-mongo-sanitize');
 
   // this is only for onboarding
   if (context.query.grub) {
       try {
+        
           const { grub } = context.query;
       
           const decoded = jwt.verify(grub, process.env.JWT_SECRET);
@@ -167,44 +167,42 @@ export async function getServerSideProps(context) {
           };
         }
   } else {
-      // general token check for the user
-      try {
-          // Get cookies from the request headers
-          const cookies = context.req.headers.cookie;
-      
-          // Parse the cookies to retrieve the otpTOKEN
-          const tokenCookie = cookies.split(';').find(c => c.trim().startsWith('token='));
-      
-          let tokenValue;
-          if (tokenCookie) {
-            tokenValue = tokenCookie.split('=')[1];
-          }
-      
-          const decoded = jwt.verify(tokenValue, process.env.USER_JWT_SECRET);
-      
-          if (decoded.type !== 'sessionToken') {
-            return {
-              redirect: {
-                destination: '/sign-in',
-                permanent: false,
-              },
-            };
-          }
-      
-          // continue rendering
-          return {
-            props: {}
-          };
-      
-      
-        } catch (error) {
-          return {
-            redirect: {
-              destination: '/sign-in',
-              permanent: false,
-            },
-          };
-        }
+    // general token check for the user
+    try {
+      // Get cookies from the request headers
+      const cookies = context.req.headers.cookie;
+  
+      // Parse the cookies to retrieve the otpTOKEN
+      const tokenCookie = cookies.split(';').find(c => c.trim().startsWith('token='));
+  
+      let tokenValue;
+      if (tokenCookie) {
+        tokenValue = tokenCookie.split('=')[1];
+      }
+  
+      const decoded = jwt.verify(tokenValue, process.env.USER_JWT_SECRET);
+  
+      if (decoded.type !== 'sessionToken') {
+        return {
+          redirect: {
+            destination: '/sign-in',
+            permanent: false,
+          },
+        };
+      }
+  
+      // continue rendering
+      return {
+        props: {}
+      };
+    } catch (error) {
+      return {
+        redirect: {
+          destination: '/sign-in',
+          permanent: false,
+        },
+      };
+    }
   }
   
   }
