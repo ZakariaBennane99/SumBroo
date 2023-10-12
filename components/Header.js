6099da3d75f3dd9c9a6c36b227ba40bf744f4173
Zakaria Bennane <zakariamonre@gmail.com>
2023-10-12 18:57:30 +0300

    fixed the unresponsivness

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import SlideMenu from './SlideMenu';
+import axios from 'axios';
 
 const Header = ({ signedIn, width, currentPath }) => {
 
@@ -10,6 +11,7 @@ const Header = ({ signedIn, width, currentPath }) => {
   const [isSignOutClicked, setIsSignOutClicked] = useState(false);
   const router = useRouter();
   const { asPath } = useRouter();
+
 
   const [localPath, setLocalPath] = useState(currentPath);
 
@@ -29,7 +31,7 @@ const Header = ({ signedIn, width, currentPath }) => {
       }
     } catch (err) {
       console.error(err);
-      setIsServerErr(true);
+      alert('Server error, please referesh the page and try again')
     }
   }
 
@@ -45,7 +47,7 @@ const Header = ({ signedIn, width, currentPath }) => {
   const renderMobileSignedInLinks = () => {
     return (
       <div id='mobile-sub-menu-wrapper'>
-        <Link href='/blog'>Blog</Link>
+        <Link href='/blog' id='blog-big-menu'>Blog</Link>
         {renderSubMenu('Dashboard', 'dashboard', isSubMenuOpen1, setIsSubMenuOpen1)}
         {renderSubMenu('Settings', 'settings', isSubMenuOpen2, setIsSubMenuOpen2)}
       </div>
@@ -76,28 +78,49 @@ const Header = ({ signedIn, width, currentPath }) => {
       </>
     );
   };
-  
-  
-  
+
 
   const renderSubMenu = (title, path, isOpen, setOpen) => {
+
     return (
       <>
-        <div onClick={() => setOpen(!isOpen)}>
-          {title}
-          <img src={localPath.includes(`/${path}`) || isOpen ? '/menu-dropper-active.svg' : '/menu-dropper.svg'} />
+        <div onClick={() => {
+            setOpen(!isOpen);
+          }} className='big-menu-items'>
+          <span style={{ color: (isOpen && path === 'dashboard') || (isOpen && path === 'settings') ? '#36366e' : '#78749c' }}>
+           {title}
+          </span> 
+          <img style={{ 
+            width: isOpen ? '20px' : '25px', 
+            height: isOpen ? '25px' : '20px',
+            transition: 'transform 0.6s ease'
+            }} src={localPath.includes(`/${path}`) || isOpen ? '/menu-dropper-active.svg' : '/menu-dropper.svg'} />
         </div>
         {(isOpen || localPath.includes(`/${path}`)) && (
           <div id='mobile-sub-menu'>
-            <Link href={`/${path}/linked-accounts`}>Linked Accounts</Link>
-            <Link href={`/${path}/account-settings`}>Account Settings</Link>
-            <Link href={`/${path}/billing`}>Billing</Link>
-            { title === 'Settings' && <button onClick={signOutUser} disabled={isSignOutClicked}>Sign Out</button> }
+            { path === 'dashboard' && 
+              <>
+                 <Link href={`/${path}/publish-a-post`}>Publish Post</Link>
+                 <Link href={`/${path}/analytics`}>Analytics</Link>
+                 <Link href={`/${path}/posts-status`}>Posts Status</Link>
+                 <Link href={`/${path}/archived-posts`}>Archive</Link>
+                 {/* Add other Dashboard specific links here */}
+              </>
+            }
+            { path === 'settings' &&
+              <>
+                 <Link href={`/${path}/linked-accounts`}>Linked Accounts</Link>
+                 <Link href={`/${path}/account-settings`}>Account Settings</Link>
+                 <Link href={`/${path}/billing`}>Billing</Link>
+                 <button id='sign-out-user' onClick={signOutUser} disabled={isSignOutClicked}>Sign Out</button>
+              </>
+            }
           </div>
         )}
       </>
     );
-  };
+};
+
 
   const renderSignedOutLinks = () => {
     return (
