4bb35b3022b9979afca638bf1d38677f9683da49
Zakaria Bennane <86924575+ZakariaBennane99@users.noreply.github.com>
2024-08-11 17:15:01 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 
 function Custom404() {
 
-  return (<div className='Error-container'>
-            <h1 style={{ fontSize: '2.5em' }}>Page Not Found</h1>
+  return (<div className="Error-container">
+            <h1 style={{ fontSize: "2.5em" }}>Page Not Found</h1>
             <img src="/404.svg" />
-            <button style={{ width: 'fit-content', 
-              padding: '15px 25px 15px 25px', 
-              fontSize: '1.5em',
-              marginBottom: '70px' }}>Go Back To The Homepage</button>
+            <button style={{ width: "fit-content", 
+              padding: "15px 25px 15px 25px", 
+              fontSize: "1.5em",
+              marginBottom: "70px" }}>Go Back To The Homepage</button>
           </div>
   );
 }
