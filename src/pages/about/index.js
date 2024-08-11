994c1b86b71f27a8edc49ac3c2df1695306045f4
Zakaria Bennane <86924575+ZakariaBennane99@users.noreply.github.com>
2024-08-11 17:13:46 +0100

    Update index.js

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -13,15 +13,15 @@ const About = () => {
                   </p>
   
                   <p>
-                  I've been exploring social media for a while, and I noticed a big problem. A lot of talented people who make great content are struggling to get noticed. But that's not all; it's also super difficult to team up with other creators. The whole process can be slow, risky, and even expensive.
+                  I&apos;ve been exploring social media for a while, and I noticed a big problem. A lot of talented people who make great content are struggling to get noticed. But that&apos;s not all; it&apos;s also super difficult to team up with other creators. The whole process can be slow, risky, and even expensive.
                   </p>
   
                   <p>
-                  That got me thinking. Why is it so hard? That's when the idea for SumBroo popped into my head. No one else seemed to be fixing this, so I rolled up my sleeves and got to work. Sure, it was a lot of effort, but my dream for SumBroo kept me going.
+                  That got me thinking. Why is it so hard? That&apos;s when the idea for SumBroo popped into my head. No one else seemed to be fixing this, so I rolled up my sleeves and got to work. Sure, it was a lot of effort, but my dream for SumBroo kept me going.
                   </p>
   
                   <p>
-                  What's SumBroo all about? It's all about bringing creators together. With SumBroo, you can easily share your work on multiple social media platforms without the usual headaches. Plus, we offer useful analytics for each of your posts. This is something you might not get if you're working with other creators the old-fashioned way.
+                  What&apos;s SumBroo all about? It&apos;s all about bringing creators together. With SumBroo, you can easily share your work on multiple social media platforms without the usual headaches. Plus, we offer useful analytics for each of your posts. This is something you might not get if you&apos;re working with other creators the old-fashioned way.
                   </p>
   
                   <p>
